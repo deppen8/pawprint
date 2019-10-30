39271e6b79f61c4a134cee3452211d07108a7a2b
Quentin CAUDRON <quentin.caudron@cbre.com>
2019-10-30 13:42:36 -0700

    Moving repos

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="pawprint",
-    version="1.1.1",
+    version="1.1.2",
     description="A flexible event tracker for rapid analysis.",
-    url="http://github.com/cbredev/pawprint",
+    url="http://github.com/qcaudron/pawprint",
     author="Quentin CAUDRON",
     author_email="quentincaudron@gmail.com",
     license="MIT",
