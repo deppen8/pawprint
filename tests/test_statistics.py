71f10e54b12337c60ff78398173056141b1ee888
Jacob Deppen <jacob.deppen@cbre.com>
2019-12-10 12:15:04 -0800

    refactor test_statistics.py tests to use fixtures

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,181 +1,126 @@
-from datetime import datetime, timedelta
-
 import numpy as np
-import pandas as pd
 
-from pawprint import Tracker, Statistics
-
-
-db = "postgresql://postgres@localhost:5432/pawprint_test_db"
-table = "pawprint_test_statistics_table"
+import pawprint
 
 
-class TestPawprintStatistics(object):
-    @classmethod
-    def setup_class(cls):
-        """If the test table exists because tests previously failed, drop it."""
+def test_sessions(pawprint_default_statistics_tracker):
+    """Test the calculation of session durations."""
 
-        # Start with a clean slate
-        try:
-            pd.io.sql.execute("DROP TABLE {}".format(table), db)
-        except:
-            pass
+    tracker = pawprint_default_statistics_tracker
+    stats = pawprint.Statistics(tracker)
 
-        # List of users who performed events
-        users = [
-            "Frodo",
-            "Frodo",
-            "Frodo",
-            "Frodo",
-            "Frodo",
-            "Frodo",
-            "Gandalf",
-            "Gandalf",
-            "Frodo",
-            "Gandalf",
-            "Gandalf",
-            "Frodo",
-            "Frodo",
-            "Frodo",
-            "Frodo",
-            "Frodo",
-        ]
+    # Calculate user session durations
+    stats.sessions()
 
-        # List of times ( minutes ) between any event and the first events
-        timedeltas = [0, 1, 2, 3, 4, 5, 100, 110, 120, 130, 140, 1000, 1001, 1002, 1003, 1004]
+    # Read the results
+    sessions = stats["sessions"].read()
 
-        # Create a tracker
-        tracker = Tracker(db=db, table=table)
-        tracker.create_table()
+    # Expected values
+    users = np.array(["Frodo", "Gandalf", "Frodo", "Frodo",])
+    durations = np.array([5, 40, 0, 4])
+    events = np.array([6, 4, 1, 5])
 
-        # Yesterday morning
-        today = datetime.now()
-        yesterday = datetime(today.year, today.month, today.day, 9, 0) - timedelta(days=1)
+    print(sessions[tracker.user_field])
 
-        # Write all events
-        for user, delta in zip(users, timedeltas):
-            tracker.write(user_id=user, timestamp=yesterday + timedelta(minutes=delta))
+    assert np.all(sessions[tracker.user_field] == users)
+    assert np.all(sessions.duration == durations)
+    assert np.all(sessions.total_events == events)
 
-        # Save the tracker
-        cls.tracker = tracker
-        cls.stats = Statistics(tracker)
+    stats.sessions(clean=False)
+    assert len(stats["sessions"].read()) == 5
 
-    @classmethod
-    def teardown_class(cls):
-        pd.io.sql.execute("DROP TABLE {}".format(table), db)
+    # Test that calculating sessions with no new data doesn't error
+    stats.sessions()
 
-    def drop_table_before_and_after(statstable):
-        def decorator(f):
-            def wrapper(self):
 
-                try:
-                    pd.io.sql.execute("DROP TABLE {}__{}".format(table, statstable), db)
-                except:
-                    pass
+def test_engagement_metrics(pawprint_default_statistics_tracker):
+    """Test the calculation of user engagement metrics."""
 
-                f(self)
+    tracker = pawprint_default_statistics_tracker
+    stats = pawprint.Statistics(tracker)
 
-                try:
-                    pd.io.sql.execute("DROP TABLE {}__{}".format(table, statstable), db)
-                except:
-                    pass
+    # Calculate user engagement
+    stats.sessions()
+    stats.engagement(min_sessions=0)
 
-            return wrapper
+    # Read the results
+    stickiness = stats["engagement"].read()
 
-        return decorator
+    # Expected values
+    dau = np.array([2, 1])
+    wau = np.array([2, 2])
+    mau = np.array([2, 2])
+    engagement = np.array([1, 0.5])
 
-    @drop_table_before_and_after("sessions")
-    def test_sessions(self):
-        """Test the calculation of session durations."""
+    assert np.all(stickiness.dau == dau)
+    assert np.all(stickiness.wau == wau)
+    assert np.all(stickiness.mau == mau)
+    assert np.all(stickiness.engagement == engagement)
+    assert set(stickiness.columns) == {"timestamp", "dau", "wau", "mau", "engagement"}
 
-        # Calculate user session durations
-        self.stats.sessions(clean=True)
 
-        # Read the results
-        sessions = self.stats["sessions"].read()
+def test_engagement_min_sessions(pawprint_default_statistics_tracker):
 
-        # Ground truth
-        users = np.array(["Frodo", "Gandalf", "Frodo", "Frodo"])
-        durations = np.array([5, 40, 0, 4])
-        events = np.array([6, 4, 1, 5])
+    tracker = pawprint_default_statistics_tracker
+    stats = pawprint.Statistics(tracker)
+    stats.sessions()
 
-        assert len(durations) == 4
-        assert np.all(sessions[self.tracker.user_field] == users)
-        assert np.all(sessions.duration == durations)
-        assert np.all(sessions.total_events == events)
+    # Now test with a minimum number of sessions
+    stats.engagement(min_sessions=2)
+    stickiness = stats["engagement"].read()
 
-        self.stats.sessions(clean=False)
-        assert len(self.stats["sessions"].read()) == 5
+    # Ground truth
+    active = np.array([1, 1])
+    engagement_active = np.array([1, 1])
+    dau = np.array([2, 1])
 
-        # Test that calculating sessions with no new data doesn't error
-        self.stats.sessions()
+    assert np.all(stickiness.dau_active == active)
+    assert np.all(stickiness.wau_active == active)
+    assert np.all(stickiness.mau_active == active)
+    assert np.all(stickiness.dau == dau)
+    assert np.all(stickiness.engagement_active == engagement_active)
+    assert set(stickiness.columns) == {
+        "timestamp",
+        "dau",
+        "wau",
+        "mau",
+        "engagement",
+        "dau_active",
+        "wau_active",
+        "mau_active",
+        "engagement_active",
+    }
 
-    @drop_table_before_and_after("engagement")
-    @drop_table_before_and_after("sessions")
-    def test_engagement(self):
-        """Test the calculation of user engagement metrics."""
+    # Test that running engagements again doesn't error if there's no new data
+    stats.engagement()
 
-        # Calculate user engagement
-        self.stats.sessions(clean=True)
-        self.stats.engagement(clean=True, min_sessions=0)
 
-        # Read the results
-        stickiness = self.stats["engagement"].read()
+def test_engagement_too_many_min_sessions(pawprint_default_statistics_tracker):
+    tracker = pawprint_default_statistics_tracker
+    stats = pawprint.Statistics(tracker)
+    stats.sessions()
 
-        # Ground truth
-        dau = np.array([2, 1])
-        wau = np.array([2, 2])
-        mau = np.array([2, 2])
-        engagement = np.array([1, 0.5])
+    # Test with too large a minimum sessions parameter
+    stats.engagement(min_sessions=20)
+    stickiness = stats["engagement"].read()
+    assert len(stickiness) == 2
+    assert set(stickiness.columns) == {"timestamp", "dau", "wau", "mau", "engagement"}
 
-        assert len(engagement) == 2
-        assert np.all(stickiness.dau == dau)
-        assert np.all(stickiness.wau == wau)
-        assert np.all(stickiness.mau == mau)
-        assert np.all(stickiness.engagement == engagement)
-        assert set(stickiness.columns) == {"timestamp", "dau", "wau", "mau", "engagement"}
 
-        # Now test with a minimum number of sessions
-        self.stats.engagement(clean=True, min_sessions=2)
-        stickiness = self.stats["engagement"].read()
+def test_engagement_append_mode(pawprint_default_statistics_tracker):
+    tracker = pawprint_default_statistics_tracker
+    stats = pawprint.Statistics(tracker)
+    stats.sessions()
 
-        # Ground truth
-        active = np.array([1, 1])
-        engagement_active = np.array([1, 1])
+    stats.engagement(min_sessions=20)
 
-        assert np.all(stickiness.dau_active == active)
-        assert np.all(stickiness.wau_active == active)
-        assert np.all(stickiness.mau_active == active)
-        assert np.all(stickiness.dau == dau)
-        assert np.all(stickiness.engagement_active == engagement_active)
-        assert set(stickiness.columns) == {
-            "timestamp",
-            "dau",
-            "wau",
-            "mau",
-            "engagement",
-            "dau_active",
-            "wau_active",
-            "mau_active",
-            "engagement_active",
-        }
+    # Try again with append-only
+    stats.engagement(clean=False)
+    stickiness = stats["engagement"].read()
+    assert len(stickiness.columns) == 5
+    assert len(stickiness) == 2
 
-        # Test that running engagements again doesn't error if there's no new data
-        self.stats.engagement()
-
-        # Test with too large a minimum sessions parameter
-        self.stats.engagement(clean=True, min_sessions=20)
-        stickiness = self.stats["engagement"].read()
-        assert len(stickiness) == 2
-        assert set(stickiness.columns) == {"timestamp", "dau", "wau", "mau", "engagement"}
-
-        # Try again with append-only
-        self.stats.engagement(clean=False)
-        stickiness = self.stats["engagement"].read()
-        assert len(stickiness.columns) == 5
-        assert len(stickiness) == 2
-
-        self.stats.engagement(clean=True, min_sessions=2)
-        stickiness = self.stats["engagement"].read()
-        assert len(stickiness) == 2
-        assert len(stickiness.columns) == 9
+    stats.engagement(clean=True, min_sessions=2)
+    stickiness = stats["engagement"].read()
+    assert len(stickiness) == 2
+    assert len(stickiness.columns) == 9
